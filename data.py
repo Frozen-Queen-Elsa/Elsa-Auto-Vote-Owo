6bb100e469efd5231fe5ba611dd69d57b381a01a
IRIS <bebebietyeu0903@gmail.com>
2022-09-24 12:38:43 +0700

    Update data.py

--- data.py
+++ data.py
@@ -1,23 +1,7 @@
-
-from requests import get
-
-
-from time import sleep
-from re import findall
-from inputimeout import TimeoutOccurred, inputimeout
-import json
-
 class data:
 	def __init__(self):
 		
 		self.wbm = [13,18]
 		self.token='Input_your_token'
-		
-
-			
-
-
-
-	
 a = data()
 
